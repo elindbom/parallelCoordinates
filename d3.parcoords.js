a60295767bfe8fbdd05c6a77078a7a498bbee9d4
elindbom <elindbom@kth.se>
2017-04-15 19:37:20 +0200

    Fixed with visualization

--- d3.parcoords.js
+++ d3.parcoords.js
@@ -9,8 +9,8 @@ d3.parcoords = function(config) {
     alphaOnBrushed: 0.0,
     mode: "default",
     rate: 20,
-    width: 700,
-    height: 200,
+    width: 600,
+    height: 300,
     margin: { top: 24, right: 0, bottom: 12, left: 0 },
     nullValueSeparator: "undefined", // set to "top" or "bottom"
     nullValueSeparatorPadding: { top: 8, right: 0, bottom: 8, left: 0 },
